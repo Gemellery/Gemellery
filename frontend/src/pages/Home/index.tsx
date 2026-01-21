f3ed5bf2c77ed2692e15579806e55435fedc94c0
Pasindu Jayasekara <147526661+PasinduJayasekara@users.noreply.github.com>
2026-01-21 12:49:55 +0530

    Home page updated

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../../components/Navbar"
+import HeroCarousel from "../../components/HeroCarousel";
 
 function Home() {
 
     return (
         <>
             <Navbar />
-            <h1 className='text-3xl font-bold underline'>Home Page</h1>
+            <HeroCarousel/>
+            {/* <h1 className='text-3xl font-bold underline'>Home Page</h1> */}
         </>
     )
 }
