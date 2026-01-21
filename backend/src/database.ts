0a235f7eec07ad5e2091244baafade538e75b928
Pasindu Jayasekara <147526661+PasinduJayasekara@users.noreply.github.com>
2026-01-21 21:49:12 +0530

    Updated

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Ukin2024#",
+  password: "prj22327",
   database: "gemellery",
   waitForConnections: true,
   connectionLimit: 10,
