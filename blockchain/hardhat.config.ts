5cf965198c166b3aa8fcb1fd2fd690dc980bfbe6
Samitha <samithabandara0305@gmail.com>
2026-03-06 06:30:32 +0530

    GEM-1: Proper confirations for hardhat to run with packeges installed

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,15 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
-// Load environment variables from .env file
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  // Solidity compiler version and optimization settings
   solidity: {
     version: "0.8.28",
     settings: {
+      evmVersion: "cancun",
+      viaIR: true,
       optimizer: {
         enabled: true,
         runs: 200,
@@ -17,18 +17,13 @@ const config: HardhatUserConfig = {
     },
   },
 
-  // Network configurations
   networks: {
-    // Sepolia testnet configuration
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.PRIVATE_KEY
-        ? [process.env.PRIVATE_KEY]
-        : [],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
 
-  // For verifying contracts on Etherscan (optional, we'll do this later)
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY || "",
   },
