ec1d3d0751f4706da4f13efccd5303cacb4eb146
Trivon <trivon.fernando180@gmail.com>
2026-03-13 22:31:47 +0530

    GEM-63 Create FAQ page base structure

---
+++ frontend/src/pages/FAQ/index.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../../components/Navbar";
+import AdvancedFooter from "../../components/AdvancedFooter";
+
+function FAQ() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <p>FAQ Page Coming Soon</p>
+      </main>
+      <AdvancedFooter />
+    </>
+  );
+}
+
+export default FAQ;
