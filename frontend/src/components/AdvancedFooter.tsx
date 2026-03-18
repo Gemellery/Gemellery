6f83b654460ad6b7d53cc8c2a5ab533ae877d09a
Trivon <trivon.fernando180@gmail.com>
2026-03-18 22:13:17 +0530

    GEM-68 Remove unused useNavigate hook from AdvancedFooter component

--- frontend/src/components/AdvancedFooter.tsx
+++ frontend/src/components/AdvancedFooter.tsx
@@ -1,6 +1,6 @@
 import logo from '../assets/logos/Elegance Jewelry.png';
 import { Instagram, Linkedin, X, Facebook } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+
 
 function AdvancedFooter() {
         
